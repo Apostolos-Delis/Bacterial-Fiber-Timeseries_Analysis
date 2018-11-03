17d563c34eee49f468368966335b58770b72f3d3
Apostolos-Delis <apost.delis@gmail.com>
2018-11-02 17:35:47 -0700

    added make_file.sh script

---
+++ bin/make_file.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ];
+then
+    echo "USAGE: make_file.sh <FILE>"
+    echo "Must have a FILE arguement"
+    exit -1
+fi
+
+FILE_NAME="../src/$1"
+
+create_py_file(){
+    echo "#!/usr/bin/env python3" > $1
+    echo "# coding: utf8" >> $1
+    echo >> $1
+    echo >> $1
+    echo "import numpy as np" >> $1
+    echo "import pandas as pd" >> $1
+    echo "import matplotlib.pyplot as plt" >> $1
+    echo "import os" >> $1
+    echo "import sys" >> $1
+    echo >> $1
+    echo "if __name__ == \"__main__\":" >> $1
+    echo "    pass" >> $1
+    echo >> $1
+}
+
+if [ ! -x $FILE_NAME ];
+then  
+    echo "ERROR: $FILE_NAME already exists"
+fi
+
+touch "$FILE_NAME"
+
+create_py_file $FILE_NAME
+chmod +x $FILE_NAME
